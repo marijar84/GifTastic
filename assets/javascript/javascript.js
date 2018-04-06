8302ccc8057761633500688bcd1cf980ddbbff1d
Maribel <maribel.jaramillo@vm2020.com>
2018-04-05 17:08:38 -0700

    Cnahges

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,7 @@
 
-var topics = ["Dragon Ball", "Los Simpsons", "Family Guy", "Rugrats", "Scooby Doo"];
+var topics = ["Dragon Ball", "The Simpsons", "The Flintstones",
+ "Tom and Jerry", "Family Guy", "Rugrats", "Scooby Doo",
+"Futurama", "Avatar", "Batman", "South Park", "Pokemon"];
 
 function drawTopics() {
     for (var item = 0; item < topics.length; item++) {
@@ -13,17 +15,22 @@ $("#submit").on("click", function (event) {
 
     event.preventDefault();
 
-    var txtAnimal = $("#inputAnimal").val().trim();
+    if ($("#inputAnimal").val().trim() != "") {
 
-    console.log(txtAnimal);
+        var txtAnimal = $("#inputAnimal").val().trim();
 
-    createButton(txtAnimal);
+        topics.push(txtAnimal);
+
+        $("#buttonAnimal").empty();
+
+        drawTopics();
+    }
 });
 
 $(document).on("click", ".clickAnimal", function (event) {
 
-
-    $("#animalDiv").empty();
+    $("#gifsDiv").empty();
+    $("#movieDiv").empty();
 
     var animal = event.currentTarget.defaultValue;
     getData(animal);
@@ -44,21 +51,20 @@ $(document).on("click", ".clickImage", function (event) {
     }
 });
 
+
 function createButton(animalType) {
     var button = $("<input>");
     button.attr("type", "submit");
     button.attr("value", animalType);
     button.addClass("clickAnimal")
-    button.addClass("btn btn-primary");
+    button.addClass("btn btn-danger");
     button.text(animalType);
     button.addClass("btnStyle");
 
     $("#buttonAnimal").append(button);
 }
 
-function getData(animalType) {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalType + "&api_key=0BQFGRWa66lLlfSA1tYVMRU1JbWey7GX&limit=10";//&rating=g";
-
+function getGifs(queryURL) {
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -71,20 +77,23 @@ function getData(animalType) {
 
         for (var item = 0; item < data.length; item++) {
 
-            if(countRow == 0 || countColumns == 3){
+            if (countRow == 0 || countColumns == 3) {
                 var row = $("<div>");
                 row.addClass("row");
                 countRow++;
                 countColumns = 0;
             }
-            
+
 
             var column = $("<div>");
             column.addClass("col-md-4");
             countColumns++;
 
             var title = $("<h2>");
-            title.text("Rating: " + data[item].rating);
+            title.text("Title: " + data[item].title);
+
+            var rating = $("<h3>");
+            rating.text("Rating: " + data[item].rating);
 
             var image = $("<img>");
             image.attr("src", data[item].images.fixed_height_still.url);
@@ -92,14 +101,73 @@ function getData(animalType) {
             image.attr("urlStill", data[item].images.fixed_height_still.url);
             image.attr("urlMoving", data[item].images.fixed_height.url);
             image.addClass("clickImage");
-            image.addClass("thumbnail")
+            image.addClass("thumbnail");
+
+
+            var download = $("<a>");
+            download.text("Download gif");
+            download.download = "image.gif";
+            download.addClass("clickDownload");
+            download.attr("target", "blank");
+            download.attr("urlMoving", data[item].images.fixed_height.url);
+            download.attr("href", data[item].images.fixed_height.url);
 
             column.append(title);
+            column.append(rating);
             column.append(image);
+            column.append(download);
 
             row.append(column);
 
-            $("#animalDiv").append(row);
+            $("#gifsDiv").append(row);
         }
     });
+}
+
+function getMovies(queryUrl) {
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        var data = response.data;
+
+        var row = $("<div>");
+        row.addClass("row");
+
+        var column = $("<div>");
+        column.addClass("col-md-12");
+
+        var title = $("<h2>");
+        console.log(response.Title);
+        title.text(response.Title);
+
+        var genre = $("<h3>");
+        console.log(response.Genre);
+        genre.text(response.Genre);
+
+        var image = $("<img>");
+        image.attr("src", response.Poster);
+        image.addClass("thumbnail");
+
+        column.append(title);
+        column.append(genre);
+        column.append(image);
+
+        row.append(column);
+
+        $("#movieDiv").append(row);
+    });
+}
+
+function getData(query) {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=0BQFGRWa66lLlfSA1tYVMRU1JbWey7GX&limit=10";
+
+    console.log(queryURL);
+    getGifs(queryURL);
+
+    var queryMovie = "https://www.omdbapi.com/?t=" + query + "&apikey=eafa5113";
+
+    getMovies(queryMovie);
 }
